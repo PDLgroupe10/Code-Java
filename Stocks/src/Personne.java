b0d199bf700f4704317a9a4a3dc23227cd194404
CrazyAuk <titoumak78@hotmail.fr>
2018-03-31 17:55:34 +0200

    Classe Personne modifiée et complétée intégralement

---
+++ Stocks/src/Personne.java
@@ -0,0 +1,100 @@
+
+/**
+ * Classe Personne
+ * @author BELOUAH - MAKOSSO
+ * @version 1.0
+ * */
+
+public class Personne {
+
+	/** 
+	 * Nom de la personne
+	 */
+	private String nom;		
+	/**
+	 * Prénom de la personne
+	 */
+	private String prenom;	
+	/**
+	 * Date de naissance de la personne
+	 */
+	private String date_nais;		
+	/**
+	 * Fonction de la personne dans l'école
+	 */
+	private String fonction;
+
+
+	/**
+	 * Constructeur
+	 * @param nom personne
+	 * @param prenom personne
+	 * @param date_nais personne
+	 * @param fonction personne
+	 */
+	public Personne(String nom, String prenom, String date_nais, String fonction) {
+		this.nom=nom;
+		this.prenom = prenom;
+		this.date_nais = date_nais;
+		this.fonction = fonction;
+	}
+	
+	/**
+	 * getter pour l'attribut nom
+	 * @return valeur du nom de la personne
+	 */
+	public String getnom() {
+		return nom;
+	}
+	/**
+	 * getter pour l'attribut prenom
+	 * @return valeur du prénom de la personne
+	 */
+	public String getprenom() {
+		return prenom;
+	}
+	/**
+	 * setter  pour l'attribut prenom
+	 * @param prenom : nouvelle valeur du prenom de la personne
+	 */
+	public void setprenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * getter  pour l'attribut date_nais
+	 * @return valeur de la date de naissance de la personne
+	 */
+	public String getdate_nais() {
+		return date_nais;
+	}
+	/**
+	 * setter  pour l'attribut date_nais
+	 * @param date_nais :  nouvelle valeur de la date de naissance de la personne
+	 */
+	public void setdate_nais(String date_nais) {
+		this.date_nais = date_nais;
+	}
+	/**
+	 * getter  pour l'attribut fonction
+	 * @return valeur de la fonction de la personne
+	 */
+	public String getfonction() {
+		return fonction;
+	}
+	/**
+	 * setter  pour l'attribut fonction
+	 * @param fonction : nouvelle valeur de la fonction de la personne
+	 */
+	public void setfonction(String fonction) {
+		this.fonction = fonction;
+	}
+
+	/**
+	 * Redéfinition de la méthode toString permettant de définir la traduction de l'objet en String
+	 * pour l'affichage par exemple
+	 */
+	public String toString() {
+		return "Personne [Nom : " + nom + " - " + prenom
+				+ ", " + date_nais + ", " + fonction + " à l'ESIGELEC]";
+	}
+}
